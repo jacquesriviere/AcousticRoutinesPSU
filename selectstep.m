573cdeea80e0e40d14004317cd5d27cea896254d
jacquesriviere <jvr5626@psu.edu>
2016-02-16 11:07:58 -0500

    Update selectstep.m
    Added one step for 'ShowMeWFs' function

--- selectstep.m
+++ selectstep.m
@@ -1,10 +1,12 @@
 function stepoptions = selectstep(step)
 
-% selectstep returns a structure based on step number and containing the appropriate step options
+% selectstep returns a structure containing the appropriate step options
+% related to the step number chosen
 
 % Init
 CHOOSE_RUN = [];
 CHECK_SYNC = [];
+SHOW_WFbefAnalysis = [];
 SHOW_allWF = [];
 SHOW_1WFperfile = [];
 SHOW_WFref = [];
@@ -18,16 +20,23 @@ switch step
     case '3'
         CHOOSE_RUN = 0;
         CHECK_SYNC = 0;
-        SHOW_WFref = 1;
+        SHOW_WFbefAnalysis = 1;
     case '4'
         CHOOSE_RUN = 0;
         CHECK_SYNC = 0;
-        SHOW_allWF = 1;
-        SHOW_1WFperfile = 0;
-        SHOW_WFref = 0;
+        SHOW_WFbefAnalysis = 0;
+        SHOW_WFref = 1;
     case '5'
         CHOOSE_RUN = 0;
         CHECK_SYNC = 0;
+        SHOW_WFbefAnalysis = 0;
+        SHOW_allWF = 1;
+        SHOW_1WFperfile = 0;
+        SHOW_WFref = 0;
+    case '6'
+        CHOOSE_RUN = 0;
+        CHECK_SYNC = 0;
+        SHOW_WFbefAnalysis = 0;
         SHOW_allWF = 0;
         SHOW_1WFperfile = 1;
         SHOW_WFref = 0;
@@ -39,6 +48,9 @@ stepoptions.CHECK_SYNC = CHECK_SYNC;
 stepoptions.SHOW_allWF = SHOW_allWF;
 stepoptions.SHOW_1WFperfile = SHOW_1WFperfile;
 stepoptions.SHOW_WFref = SHOW_WFref;
+stepoptions.SHOW_WFbefAnalysis = SHOW_WFbefAnalysis;
 
 end
+
+
 
