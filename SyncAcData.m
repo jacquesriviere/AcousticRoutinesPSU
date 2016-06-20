80ac7ea2dd8d976aeb11e2a9b81276468b9f7c0c
jacquesriviere <jvr5626@psu.edu>
2016-06-20 14:39:13 -0400

    Update SyncAcData.m

--- SyncAcData.m
+++ SyncAcData.m
@@ -22,20 +22,20 @@ function [acTime,acRate_adjusted,ts_adjusted,TotalNumberOfFiles] = SyncAcData(Ac
 % OUTPUTS
 % acTime is the time vector for acoustic data obtained after synchronization
 % acRate_adjusted is the rate at which acoustic pulses are sent (found after adjusting using biax recorder as a reference)
-% ts_adjusted is the sampling period (i.e. 1/fs) found after adjusting using biax recorder as a reference
+% ts_adjusted is the sampling period in microsec (i.e. 1/fs) found after adjusting using biax recorder as a reference
 % TotalNumberOfFiles is the total number of acoustic files to be analyzed 
 
 % acoustic parameters
-acSettings = load(Ac_path);                                 % load acoustic settings
-acPeriod = acSettings.timeBetweenpulses/1e3;                % time btw pulses
-acRate = 1/acPeriod;                                        % in Hz (number of WF per second)
-numSFpfile = acSettings.numFrames/2;                        % number of superframes per file
-numWFpSFpCH = acSettings.numAcqs;                           % number of WF per superframe and per channel
-numWFpfilepCH = numSFpfile*numWFpSFpCH;                     % number of WF per file and per channel
-fs = acSettings.samplingFreq*1e6;                           % acoustic sampling rate
+acSettings = load(Ac_path);                     % load acoustic settings
+acPeriod = acSettings.SeqControl(1).argument;   % time btw pulses in microsec (SeqControl(1).argument = timeBetweenpulses)
+acRate = 1e6/(acPeriod);                        % in Hz (number of WF per second)
+numSFpfile = acSettings.numFrames/2;            % number of superframes per file
+numWFpSFpCH = acSettings.numAcqs;               % number of WF per superframe and per channel
+numWFpfilepCH = numSFpfile*numWFpSFpCH;         % number of WF per file and per channel
+fs = acSettings.samplingFreq;                   % acoustic sampling rate in MHz
 clear acSettings
 
-ts = 1/fs; % sampling time
+ts = 1/fs; % sampling time in microsec
 
 MAX = max(Sync(idxft:idxlt));
 MIN = min(Sync(idxft:idxlt));
@@ -46,11 +46,10 @@ Sync(idxft:idxlt) = Sync(idxft:idxlt) - max(Sync(idxft:idxlt));
 figure;
 plot(Time(idxft:idxlt),Sync(idxft:idxlt));hold on
 xlabel('Time (s)');ylabel('Normalized Sync');
-dcmObj = datacursormode;
-set(dcmObj,'UpdateFcn',@NewCallback);
+dcmObj = datacursormode;set(dcmObj,'UpdateFcn',@GoodCursor);
 
 % theoretical time between two consecutive triggers
-t_btw_trig = numWFpfilepCH/acRate; % time in s
+t_btw_trig = numWFpfilepCH*acPeriod/1e6; % time in sec
 
 % build a trigger time vector using the large trigger chosen above as a reference
 
@@ -58,25 +57,26 @@ t_btw_trig = numWFpfilepCH/acRate; % time in s
 % this number should be an integer if no mismatch occurs between mechanical and acoustical recorders.
 % In the following, we take the biax recorder as the reference and adjust
 % the acoustical data to match the mechanical data.
-Nspans = (Time(idxref2) - Time(idxref1))/(t_btw_trig);
+Nspans = (Time(idxref2) - Time(idxref1))/t_btw_trig;
+
 % actual time between two consecutive triggers (using biax recorder as a reference)
-actual_t_btw_trig = (Time(idxref2) - Time(idxref1))/floor(Nspans);
+actual_t_btw_trig = (Time(idxref2) - Time(idxref1))/round(Nspans); % sec   (round instead of floor here...)
 format long
-mismatchPRF = (actual_t_btw_trig - t_btw_trig)/numWFpfilepCH;
-acPeriod_adjusted = acPeriod+mismatchPRF;
-acRate_adjusted = 1/(acPeriod_adjusted);
+mismatchPRF = (actual_t_btw_trig - t_btw_trig)*1e6/numWFpfilepCH;
+acPeriod_adjusted = acPeriod+mismatchPRF; % microsec
+acRate_adjusted = 1e6/(acPeriod_adjusted); % Hz
 
-fprintf(['In theory, acoustic pulses are sent every ' num2str(1/acRate*1e3,15) ' ms.\n' ...
-         'Using biax recorder as a reference, we find ' num2str(acPeriod_adjusted*1e3,15) ' ms.\n' ...
-         'Mismatch for the acoustic pulsing rate is ' num2str(mismatchPRF*1e9) ' ns.\n\n']);
- 
-mismatchts = mismatchPRF/(fs/acRate);
-fs_adjusted = 1/(ts+mismatchts); % adjusted sampling frequency
-ts_adjusted = 1/fs_adjusted;
+fprintf(['In theory, acoustic pulses are sent every ' num2str(acPeriod/1e3,15) ' ms.\n' ...
+         'Using biax recorder as a reference, we find ' num2str(acPeriod_adjusted/1e3,15) ' ms.\n' ...
+         'Mismatch for the acoustic pulsing rate is ' num2str(mismatchPRF*1e3) ' ns.\n\n']);
 
-fprintf(['Said differently, sampling frequency is ' num2str(fs/1e6) ' MHz in theory.\n' ...
-         'Using biax recorder as a reference, it is adjusted to ' num2str(fs_adjusted/1e6,10) ' MHz.\n' ...
-         'Mismatch for sampling time is ' num2str(mismatchts*1e12) ' ps.\n\n']);
+mismatchts = (acRate/1e6)/fs*mismatchPRF; % microsec * MHz/MHz = microsec
+ts_adjusted = ts+mismatchts; % adjusted sampling period (microsec)
+fs_adjusted = 1/ts_adjusted; % adjusted sampling frequency (MHz)
+
+fprintf(['Said differently, sampling frequency is ' num2str(fs) ' MHz in theory.\n' ...
+         'Using biax recorder as a reference, it is adjusted to ' num2str(fs_adjusted,10) ' MHz.\n' ...
+         'Mismatch for sampling time is ' num2str(mismatchts*1e6) ' ps.\n\n']);
      
 % idxref2 is used as a reference to build sync vectors
 % raw sync, using theoretical t_btw_trig
@@ -111,17 +111,27 @@ trigsraw = -0.5*ones(Ntriggerraw,1); % -0.5 to be adjusted depending on the expe
 trigs = -0.51*ones(Ntrigger,1); 
 
 % plot raw and adjusted sync, as well as the two reference triggers
+figure(2)
 plot(gca,trigger_timeraw,trigsraw,'*');
 plot(gca,trigger_time,trigs,'*');
 plot(gca,Time(idxref1),-0.52,'s');
 plot(gca,Time(idxref2),-0.52,'s');
 set(gca,'FontSize',16);
-dcmObj = datacursormode;
-set(dcmObj,'UpdateFcn',@GoodCursor);
+dcmObj = datacursormode;set(dcmObj,'UpdateFcn',@GoodCursor);
 legend('Sync Signal','Raw Sync','Adjusted Sync (using biax recorder as reference)', ...
     'First Reference Trigger','Second Reference Trigger')
 
+% Find peaks in the sync data (will work when recording rate is 1000Hz or higher)
+% uncomment to see the experimental peaks...
+% [pks,locs] = findpeaks(-Sync(idxft:idxlt),'MinPeakDistance',t_btw_trig*980); 
+% % 980 is slightly lower than 1000Hz, the sampling rate used for this run
+% locs = locs + idxft - 1;
+% plot(Time(locs),Sync(locs),'sm');hold on
+
 figure;
+% plot(diff(Time(locs))*1000,'Marker','.');hold on % uncomment to see the
+% experimental peaks...
+
 plot(check*1000,'r');hold on
 plot(check2*1000,'g');title('Time between consecutive triggers should be constant');
 xlabel('Index Number');
@@ -135,8 +145,8 @@ fprintf(['Check that the time between consecutive triggers is constant,\n' ...
 acN = TotalNumberOfFiles*numWFpfilepCH; % total number of WF per channel
 
 % build acoustic time vector
-acTime = 0:acPeriod_adjusted:acPeriod_adjusted*(acN-1);
-acTime = acTime + trigger_time(1);
+acTime = (0:acPeriod_adjusted:acPeriod_adjusted*(acN-1))/1e6; % seconds
+acTime = acTime + trigger_time(1);                            % seconds
 
 % figure to check that 'acTime' vector is correct (it should overlap 'trigger_time' vector)
 % figure; 
@@ -148,6 +158,4 @@ acTime = acTime + trigger_time(1);
 
 
 end
-
-
 
